d90a5fb2793c891b0715362f707351d096ea51dd
junseyeon <44864717+junseyeon@users.noreply.github.com>
2021-11-29 17:29:48 +0900

    now : csv to sql

--- main/models.py
+++ main/models.py
@@ -2,17 +2,23 @@ from django.db import models
 
 
 class MovieModel(models.Model):
-    show_id = models.TextField()
-    type = models.TextField()
-    title = models.TextField()
-    director = models.TextField()
-    cst = models.TextField()
-    country = models.CharField(max_length=50)
-    release_year = models.IntegerField
-    rating = models.CharField(max_length= 30)
+    show_id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    cast = models.TextField()
+    country = models.CharField(max_length=200)
+    release_year = models.IntegerField(null=True)
+    rating = models.CharField(max_length=30)
     duration = models.CharField(max_length=60)
     listed_in = models.TextField()
     description = models.TextField()
 
     def __str__(self):
-        return self.show_id
+        return self.show_id + self.title
+
+    class Meta:
+        db_table ='moviemodel'
+
+
+
--- main/views.py
+++ main/views.py
@@ -1,2 +1,35 @@
 from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.http import HttpResponse
+
+import csv
+
+from .models import MovieModel
+
+
+# def index(request):
+#     return render(request, 'index.html')
+
+
+def csvToModel(request):
+
+    #
+    # path = "renew_nefilx_titles.csv"
+    # file = open(path)
+    # reader = csv.reader(file)
+    # print("----", reader)    # 파일 읽어 왔는지 확인
+    #
+    # list = []
+    # for row in reader:
+    #     list.append(MovieModel(show_id=row[0],
+    #                            type =row[1],
+    #                            title=row[2],
+    #                            director=row[3],
+    #                            cast=row[4],
+    #                            country=row[5],
+    #                            release_year=row[6],
+    #                            rating=row[7],
+    #                            duration=row[8],
+    #                            listed_in=row[9],
+    #                            description=row[10]))
+
+    return HttpResponse('create model')
